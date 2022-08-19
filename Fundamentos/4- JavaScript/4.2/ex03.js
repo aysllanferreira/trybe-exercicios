2b1b1f6ba59b226ded1992f1c56fa28bd1b5b9d1
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-08-19 16:29:28 -0300

    ex03 feito

---
+++ Fundamentos/4- JavaScript/4.2/ex03.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let c = 0; c < numbers.length; c++) {
+  soma = soma + numbers[c];
+}
+
+let media = soma / numbers.length;
+
+console.log(media);
