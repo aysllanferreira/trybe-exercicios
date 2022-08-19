8004d73624ccdd7e8f049ca6697ce94a495553d2
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-08-19 16:29:52 -0300

    ex04 feito

---
+++ Fundamentos/4- JavaScript/4.2/ex04.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let c = 0; c < numbers.length; c++) {
+  soma = soma + numbers[c];
+}
+
+let media = soma / numbers.length;
+
+if (media > 20) {
+  console.log("Valor maior que 20");
+} else {
+  console.log("Valor menor ou igual a 20");
+}
