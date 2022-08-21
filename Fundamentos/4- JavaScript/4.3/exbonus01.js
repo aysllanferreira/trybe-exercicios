dc40c2b2a3abba89c6a2d9b2831831ea13837be2
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-08-21 08:31:34 -0300

    bonus 01 feito

--- Fundamentos/4- JavaScript/4.3/exbonus01.js
+++ Fundamentos/4- JavaScript/4.3/exbonus01.js
@@ -1,3 +1,14 @@
-let num = 5;
-let simbolo = "*";
-let espacinho = "";
+let n = 7;
+let ast = "";
+
+if (n <= 0) {
+  console.log("Erro! Digite um número maior que 0.");
+} else {
+  for (let i = 0; i < n; i++) {
+    for (let x = 0; x < n; x++) {
+      ast += "*";
+    }
+    console.log(ast);
+    ast = "";
+  }
+}
