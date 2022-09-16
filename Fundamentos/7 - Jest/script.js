f3bec68cccc9fad133c621f243d18283b8ca7e90
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-16 16:17:09 -0300

    Verifica espacoes em branco

--- Fundamentos/7 - Jest/script.js
+++ Fundamentos/7 - Jest/script.js
@@ -4,9 +4,21 @@ const verifyNanNumber = (result) => {
     }
 }
 
+const verifyBlankSpaces = (value1, value2) => {
+    if (value1.trim() === '' || value2.trim() === '') {
+        throw new Error('Os campos nao podem estar em branco');
+    }
+}
+
 const sum = () => {
     const value1 = document.getElementById('value1').value;
     const value2 = document.getElementById('value2').value;
+    try {
+        verifyBlankSpaces(value1, value2);
+    } catch (error) {
+        return alert(error.message)
+    }
+
     const result = Number(value1) + Number(value2);
     console.log(result);
     try {
@@ -16,6 +28,7 @@ const sum = () => {
         document.getElementById('value2').value = '';
         return alert('Os valores precisam ser numericos!');
     }
+
     document.getElementById('result').innerHTML = `Resultado: ${result}`;
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
