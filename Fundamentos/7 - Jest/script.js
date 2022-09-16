152bbe3fa96cc8da5b30daaa81785c79dcd1b6eb
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-16 16:13:07 -0300

    Verificacao se o numero eh Nan implementado

--- Fundamentos/7 - Jest/script.js
+++ Fundamentos/7 - Jest/script.js
@@ -1,11 +1,21 @@
-const verifyBlankSpaces = () => {
-    
+const verifyNanNumber = (result) => {
+    if (isNaN(result)) {
+        throw new Error('Os valores precisam ser numericos');
+    }
 }
 
 const sum = () => {
     const value1 = document.getElementById('value1').value;
     const value2 = document.getElementById('value2').value;
     const result = Number(value1) + Number(value2);
+    console.log(result);
+    try {
+        verifyNanNumber(result);
+    } catch (error) {
+        document.getElementById('value1').value = '';
+        document.getElementById('value2').value = '';
+        return alert('Os valores precisam ser numericos!');
+    }
     document.getElementById('result').innerHTML = `Resultado: ${result}`;
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
