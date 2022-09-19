7bd767097685e0d114f6db840371f1dec1f7b808
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-19 12:47:10 -0300

    Refatorando codigo

--- Fundamentos/7 - Jest/dia3/techList.js
+++ Fundamentos/7 - Jest/dia3/techList.js
@@ -2,17 +2,16 @@ const techList = (tech, nome) => {
   const naOrdem = tech.sort();
   let objeto;
   const arrayvazio = [];
-  for (let i = 0; i < tech.length; i += 1) {
+
+  tech.forEach((element) => {
     objeto = {
       name: nome,
-      tech: naOrdem[i],
+      tech: element,
     };
     arrayvazio.push(objeto);
-  }
+  });
 
-  if (arrayvazio.length === 0) {
-    return "Vazio!";
-  }
+  if (arrayvazio.length === 0) return "Vazio!";
 
   return arrayvazio;
 };
